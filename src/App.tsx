10a92af6af2330d8ffe07205951a6716c3ad54c3
Vitor Bahir <vitorb82014@gmail.com>
2025-06-15 23:54:31 -0300

    Propriedades (props) e children de componentes no React

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,11 @@ export function App() {
 
   return (
     <>
-      <Heading />
+      <Heading attr={123} attr2='String'>
+        Olá mundo 1
+      </Heading>
+      <Heading>Olá mundo 2</Heading>
+      <Heading>Olá mundo 3</Heading>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam nesciunt
         illum recusandae excepturi nam perferendis. Eligendi possimus, nulla
