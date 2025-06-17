5de2826abcbfa2202b51ec831931d4e90f25eefe
Vitor Bahir de Andrade <105059969+VitorBahir@users.noreply.github.com>
2025-06-16 23:52:06 -0300

    Estrutura HTML e CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam nesciunt
-        illum recusandae excepturi nam perferendis. Eligendi possimus, nulla
-        harum dignissimos, nihil, magni molestias officia vel voluptate commodi
-        minima velit fugit.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
