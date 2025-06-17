796223d7f8cfec2c33ea27e1e716a20fd98c70bc
Vitor Bahir de Andrade <105059969+VitorBahir@users.noreply.github.com>
2025-06-17 01:29:07 -0300

    Componente de menu com link e icone

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -9,12 +9,10 @@ export function App() {
   return (
     <>
       <Container>
-        <Heading>
-          <Logo />
-        </Heading>
+        <Logo />
       </Container>
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
     </>
   );
