6b953006db26ef4daf0c17b6eaa027e2f3535e0a
Vitor Bahir de Andrade <105059969+VitorBahir@users.noreply.github.com>
2025-06-17 02:30:30 -0300

    CSS no input ativo, em foco e desativado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -25,7 +25,12 @@ export function App() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-            <DefaultInput id='meuInput' type='text' labelText='Task' />
+            <DefaultInput
+              id='meuInput'
+              type='text'
+              labelText='Task'
+              placeholder='Digite algo'
+            />
           </div>
 
           <div className='formRow'>
